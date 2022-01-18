ae305ddc425b6881bc759bca92e4d7bd1189a4ce
Ivan Martini <ivanferrarimartini@yahoo.com.br>
2022-01-18 08:37:20 -0300

    User entity model

--- src/User/infra/typeorm/entities/User.ts
+++ src/User/infra/typeorm/entities/User.ts
@@ -1,7 +1,34 @@
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+
 export class User {
-  id: number;
-  name: string;
-  age: number;
+  id: string;
+
+  @IsNotEmpty({
+    message: 'First name is required.',
+  })
+  @IsString({
+    message: 'First name must have only words.',
+  })
+  firstName: string;
+
+  @IsNotEmpty({
+    message: 'Last name is required.',
+  })
+  lastName: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'Email needs to be valid.',
+    },
+  )
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password is required.',
+  })
+  password: string;
+
   createdAt: Date;
   updatedAt: Date;
 }
