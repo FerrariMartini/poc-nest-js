94f1454070bd5266ef52bd001f697c4893cfed96
Ivan Martini <ivanferrarimartini@yahoo.com.br>
2022-01-18 08:39:47 -0300

    User services modules

--- src/User/services/UserCreate.ts
+++ src/User/services/UserCreate.ts
@@ -1,42 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
-
-interface dbUserModel {
-  id: number;
-  name: string;
-  age: number;
-}
-
-const dbUsers = [
-  {
-    id: 12313,
-    name: 'Ivan',
-    age: 38,
-    createdAt: '2022-01-14T18:34:03.331Z',
-    updatedAt: '2022-01-14T18:34:03.331Z',
-  },
-  {
-    id: 2,
-    name: 'Dino',
-    age: 14,
-    createdAt: '2022-01-14T18:34:03.331Z',
-    updatedAt: '2022-01-14T18:34:03.331Z',
-  },
-];
-
+import { User } from '../infra/typeorm/entities/User';
+import { saveUser } from '../repositories/fake/UserRepositories';
 @Injectable()
 export class UserCreate {
-  createNewUser(newUser): any {
-    dbUsers.push({
-      id: uuid(),
-      ...newUser,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    return dbUsers[dbUsers.length - 1];
-  }
-
-  getAllUsers(): dbUserModel[] {
-    return dbUsers;
+  createNewUser(newUser: User): User {
+    return saveUser(newUser);
   }
 }
--- src/User/services/UserShow.ts
+++ src/User/services/UserShow.ts
@@ -1,31 +1,17 @@
-import { Injectable } from "@nestjs/common";
-
-interface dbUserModel {
-  id: number;
-  name: string;
-  age: number;
-}
-
-const dbUsers = [
-  {
-    id: 12313,
-    name: 'Ivan',
-    age: 38,
-    createdAt: '2022-01-14T18:34:03.331Z',
-    updatedAt: '2022-01-14T18:34:03.331Z',
-  },
-  {
-    id: 2,
-    name: 'Dino',
-    age: 14,
-    createdAt: '2022-01-14T18:34:03.331Z',
-    updatedAt: '2022-01-14T18:34:03.331Z',
-  },
-];
+import { Injectable } from '@nestjs/common';
+import { User } from '../infra/typeorm/entities/User';
+import {
+  getAllUsers,
+  getUserById,
+} from '../repositories/fake/UserRepositories';
 
 @Injectable()
 export class UserShow {
-  getAllUsers(): dbUserModel[] {
-    return dbUsers;
+  getAllUsers(): User[] {
+    return getAllUsers();
+  }
+
+  getById(id: string): User {
+    return getUserById(id);
   }
 }
