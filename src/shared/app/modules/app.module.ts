62a3a6b07c0edffddeb5feaff9bd577c78943374
Ivan Martini <ivanferrarimartini@yahoo.com.br>
2022-02-10 09:31:48 -0300

    Set global seralizier interceptor up

--- src/shared/app/modules/app.module.ts
+++ src/shared/app/modules/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UserModule } from '../../../modules/User/UserModule';
 import { AppController } from '../infra/https/controllers/app.controller';
 import { AppService } from '../services/app.service';
@@ -7,6 +8,12 @@ import { AppService } from '../services/app.service';
   imports: [UserModule],
   exports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
